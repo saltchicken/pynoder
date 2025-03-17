6e7ad8f76853b842c76e9180ee335c8142e53e05
saltchicken <john.eicher89@gmail.com>
2025-03-16 21:46:29 -0700

    Add save/load graph and node parser

---
+++ nodes.js
@@ -0,0 +1,27 @@
+//node constructor class
+function MyAddNode()
+{
+  this.addInput("A","number");
+  this.addInput("B","number");
+  this.addOutput("A+B","number");
+  this.properties = { precision: 1 };
+}
+
+//name to show
+MyAddNode.title = "Sum";
+
+//function to call when the node is executed
+MyAddNode.prototype.onExecute = function()
+{
+  var A = this.getInputData(0);
+  if( A === undefined )
+    A = 0;
+  var B = this.getInputData(1);
+  if( B === undefined )
+    B = 0;
+  this.setOutputData( 0, A + B );
+}
+
+//register in the system
+LiteGraph.registerNodeType("basic/sum", MyAddNode );
+
--- server.py
+++ server.py
@@ -2,14 +2,24 @@ from aiohttp import web
 import json
 from pprint import pprint
 
+class Node:
+    def __init__(self, node):
+        self.flags = node.get('flags', None)
+        self.mode = node.get('mode', None)
+        self.order = node.get('order', None)
+        self.inputs = node.get('inputs', None)
+        self.outputs = node.get('outputs', None)
+        self.properties = node.get('properties', None)
+        self.type = node.get('type', None)
+
 app = web.Application()
 
 async def process_graph(request):
     """Handles POST requests from the frontend with graph data."""
     data = await request.json()
-    nodes = data['nodes']
+    nodes = [Node(node) for node in data['nodes']]
     for node in nodes:
-        pprint(node)
+        pprint(node.__dict__)
     return web.json_response({"status": "success", "message": "Graph data received."})
 
 app.router.add_post('/process_graph', process_graph)
